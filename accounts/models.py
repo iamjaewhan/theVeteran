cabfe5bc2b0f8ad95f9196f2ab05893b96c31eb0
jay <jaewhan232@naver.com>
2022-01-19 20:17:40 +0900

    feat : User model 생성

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
 
 # Create your models here.
 
-class User():
-    def 
+class User(AbstractBaseUser):
+    user_id = models.CharField(max_length=50, Null=False, unique=True)
+    
+    def __str__(self):
+        return self.user_id
