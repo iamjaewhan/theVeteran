6023c8f781d5145e0c9f89e268df472be1016c4d
jay <jaewhan232@naver.com>
2022-01-26 16:46:43 +0900

    feat : create CustomUserManager

--- accounts/models.py
+++ accounts/models.py
@@ -1,18 +1,29 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager, UserManager
 from django.utils import timezone
 
 # Create your models here.
+class CustomUserManager(UserManager):
+    def create_user(self, user_id, password=None):
+        if not user_id:
+            raise ValueError('create_user() missing user id')
+        user = self.model(user_id=user_id)
+        user.set_password(password)
+        user.save()
+        return user
+    
+    def create_superuser(self, user_id, password):
+        return self.create_user(user_id, password)
 
 class User(AbstractBaseUser):
-    user_id = models.CharField(max_length=50, unique=True)
+    user_id = models.CharField(max_length=50, unique=True, verbose_name='아이디')
     
     USERNAME_FIELD = 'user_id'
     
+    objects = CustomUserManager()
+    
     def __str__(self):
         return self.user_id
-    
-    
     
 class Host(models.Model):
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
