466c44451fec7462111ead4fb509429ab4462319
jay <jaewhan232@naver.com>
2022-01-31 14:58:26 +0900

    feat:경기-참가자 모델 생성

--- game/models.py
+++ game/models.py
@@ -7,4 +7,18 @@ class Game(models.Model):
     host = models.ForeignKey('accounts.Host', on_delete=models.CASCADE)
     start_datetime = models.DateTimeField()
     end_datetime = models.DateTimeField()
+    numOfRec = models.IntegerField(null=False, default=5)
     
+    def is_Progressed(self):
+        if self.start_datetime > timezone.localtime():
+            return False
+        else:
+            return True
+        
+    
+class Game_Participants(models.Model):
+    game = models.ForeignKey(Game, on_delete=models.RESTRICT)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)   
+
+    class Meta:
+        unique_together = (('game','user'))
